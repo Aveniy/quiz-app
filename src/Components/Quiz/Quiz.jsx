de93b21dcb41aead6d508bf4f43a58b8645624a8
unknown <abebeabenezer31@gmail.com>
2025-08-01 15:59:55 +0300

    in commit

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Quiz.css'
+ 
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+      <h1>Quiz app</h1>
+      <P></P>
+    </div>
+      
+  )
+}
+
+export default Quiz
