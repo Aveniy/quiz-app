e2c84ff1debfead6dd86a6a642041cec33da592f
unknown <abebeabenezer31@gmail.com>
2025-08-02 20:57:10 +0300

    adding functionality to q&a page

---
+++ src/assets/Data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    question: "What is the capital of France?",
+    option1: "Berlin",
+    option2: "Madrid",
+    option3: "Paris",
+    option4: "Rome",
+    ans: 3,
+  },
+  {
+    question: "Which language runs in a web browser?",
+    option1: "Java",
+    option2: "C",
+    option3: "Python",
+    option4: "JavaScript",
+    ans: 4,
+  },
+  {
+    question: "What does CSS stand for?",
+    option1: "Central Style Sheets",
+    option2: "Cascading Style Sheets",
+    option3: "Coded Style Syntax",
+    option4: "Computer Style System",
+    ans: 2,
+  },
+  {
+    question: "Which HTML tag is used to define an unordered list?",
+    option1: "<ol>",
+    option2: "<ul>",
+    option3: "<li>",
+    option4: "<list>",
+    ans: 2,
+  },
+  {
+    question: "What does React primarily use to build UI?",
+    option1: "Templates",
+    option2: "Classes",
+    option3: "Components",
+    option4: "Functions",
+    ans: 3,
+  }
+];
+
+
